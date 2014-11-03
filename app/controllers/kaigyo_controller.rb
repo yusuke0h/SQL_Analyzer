f3c11d3811802731a97a44d1708d10e138700910
Yusuke Hosoi <yusuke0.hosoi@gmail.com>
2014-11-03 22:52:19 +0900

    ロジック変更

--- app/controllers/kaigyo_controller.rb
+++ app/controllers/kaigyo_controller.rb
@@ -1,24 +1,64 @@
 class KaigyoController < ApplicationController
   def index
     if params[:data]
-      sql = params[:data][:sql]
-      analyzed_sal = sql_analyze sql
-      @processed_text = sql_textnize(sql, analyzed_sal).gsub("\n", "<br>")
+      @sql = params[:data][:sql]
+      @analyzed_sal = sql_analyze @sql
+      @sql_change = {
+        fields: "SELECT",
+        tables: "FROM",
+        terms: "WHERE",
+        etc: "Other",
+      }
+      @tables = Hash.new(0)
+      @analyzed_sal[:tables].each do |from_sentence|
+        from_terms = from_sentence.split.map { |e| e.strip }
+        from_terms.each_with_index do |term, index|
+          case from_terms[index-1]
+          when /FROM/
+            @tables[term] += 1
+          when /JOIN/
+            @tables[term] += 1
+          end
+        end
+      end
     end
   end
 
   private
   def sql_analyze sql
-    select, from, where = sql.split(/SELECT|FROM|WHERE/).reject{ |e| e == "" }.map { |e| e.strip }
+    sql_funcs = ["SELECT", "FROM", "WHERE", "ORDER BY", "GROUP BY", "LIMIT"]
+    replaced_sql = sql
+    sql_funcs.each do |sql_func|
+      replaced_sql = replaced_sql.gsub(sql_func, "*****#{sql_func}")
+    end
+
+    etc = []
+    select, from, where = ["", "", ""]
+    sentences = replaced_sql.split("*****").reject{ |e| e.strip == "" }.map { |e| e.strip }
+    sentences.each do |sentence|
+      case sentence
+      when /SELECT/
+        select << sentence
+        select << ','
+      when /FROM/
+        from << sentence
+        from << ','
+      when /WHERE/
+        where << sentence
+        where << '******'
+      else
+        etc << sentence
+      end
+    end
 
     fields = select.split(",").map { |e| e.strip }
     tables = from.split(",").map { |e| e.strip }
-    tmp_terms = where.split(/AND|OR/)
+    tmp_terms = where.gsub("AND", "******AND").gsub("OR", "******OR").split("******")
     terms = []
     next_flg = false
     tmp_terms.each_with_index do |term, index|
       if term.include?("between")
-        terms << [term.strip, tmp_terms[index + 1].strip].join(" AND ")
+        terms << [term.strip, tmp_terms[index + 1].strip].join(" ")
         next_flg = true
       else
         if next_flg
@@ -28,7 +68,7 @@ class KaigyoController < ApplicationController
         terms << term.strip
       end
     end
-    return {fields: fields, tables: tables, terms: terms}
+    return {fields: fields, tables: tables, terms: terms, etc: etc}
   end
 
   def sql_textnize sql, analyzed_sal
@@ -61,6 +101,14 @@ class KaigyoController < ApplicationController
     output_text << '===WHERE terms==='
     output_text << "\n"
     analyzed_sal[:terms].each{|e|
+      output_text << e
+      output_text << "\n"
+    }
+
+    output_text << "\n"
+    output_text << '===Others==='
+    output_text << "\n"
+    analyzed_sal[:etc].each{|e|
       output_text << e
       output_text << "\n"
     }
